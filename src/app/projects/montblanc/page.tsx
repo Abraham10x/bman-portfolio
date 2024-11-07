30ae0745070accf49ec0c260eba89d76a874d329
Ameh Abraham <amehabraham811@gmail.com>
2024-11-07 04:52:49 +0100

    worked on montblanc

--- src/app/projects/montblanc/page.tsx
+++ src/app/projects/montblanc/page.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 const page = () => {
   const designs = [
-    "/assets/projects/polo-limited/new-year.png",
-    "/assets/projects/polo-limited/rolex-novelties.png",
-    "/assets/projects/polo-limited/valentines-day.png",
-    "/assets/projects/polo-limited/fathers-day-artboard.png",
-    "/assets/projects/polo-limited/cartier-artboard.png",
-    "/assets/projects/polo-limited/sea-dweller-artboard.png",
+    "/assets/projects/montblanc/fathers-day.png",
+    "/assets/projects/montblanc/eid-maulud.png",
+    "/assets/projects/montblanc/workers-day.png",
+    "/assets/projects/montblanc/fathers-day-artboard.png",
+    "/assets/projects/montblanc/independence-day.png",
+    "/assets/projects/montblanc/merry-christmas.png",
   ];
   return (
     <div className="xl:container pt-10 sm:pt-14 px-5 sm:px-10 mx-auto">
-      <Title header="Polo Limited" />
+      <Title header="Montblanc" />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-32">
         {designs.map((item, index) => (
           <Image
@@ -27,14 +27,14 @@ const page = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-3.5">
         <Image
           className="w-full"
-          src="/assets/projects/polo-limited/rolex-watches.png"
+          src="/assets/projects/montblanc/tech-gadgets.png"
           alt="swarovski flyer"
           width={500}
           height={500}
         />
         <Image
           className="w-full"
-          src="/assets/projects/polo-limited/iconic-watches.png"
+          src="/assets/projects/montblanc/happy-democracy.png"
           alt="swarovski flyer"
           width={500}
           height={500}
